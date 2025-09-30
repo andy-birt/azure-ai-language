72b71aff31e3c0c80dec855434d52747c354dd32
andy-birt <abirt1986@gmail.com>
2025-09-30 23:47:05 +0000

    extra code for reference

--- Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
+++ Labfiles/07-speech/Python/speaking-clock/speaking-clock.py
@@ -40,6 +40,24 @@ def TranscribeCommand():
     audio_config = speech_sdk.AudioConfig(filename=audioFile)
     speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
 
+    # # Configure speech recognition
+    # audio_config = speech_sdk.AudioConfig(use_default_microphone=True)
+    # speech_recognizer = speech_sdk.SpeechRecognizer(speech_config, audio_config)
+    # print('Speak now...')
+
+    # # Process speech input
+    # speech = speech_recognizer.recognize_once_async().get()
+    # if speech.reason == speech_sdk.ResultReason.RecognizedSpeech:
+    #     command = speech.text
+    #     print(command)
+    # else:
+    #     print(speech.reason)
+    #     if speech.reason == speech_sdk.ResultReason.Canceled:
+    #         cancellation = speech.cancellation_details
+    #         print(cancellation.reason)
+    #         print(cancellation.error_details)
+
+
     # Process speech input
     print("Listening...")
     speech = speech_recognizer.recognize_once_async().get()
@@ -67,6 +85,18 @@ def TellTime():
     speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
     audio_config = speech_sdk.audio.AudioConfig(filename=output_file)
     speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config, audio_config,)
+
+    # response_text = 'The time is {}:{:02d}'.format(now.hour,now.minute)
+
+    # # Configure speech synthesis
+    # speech_config.speech_synthesis_voice_name = "en-GB-RyanNeural"
+    # audio_config = speech_sdk.audio.AudioOutputConfig(use_default_speaker=True)
+    # speech_synthesizer = speech_sdk.SpeechSynthesizer(speech_config, audio_config)
+
+    # # Synthesize spoken output
+    # speak = speech_synthesizer.speak_text_async(response_text).get()
+    # if speak.reason != speech_sdk.ResultReason.SynthesizingAudioCompleted:
+    #     print(speak.reason)
 
     # Synthesize spoken output
     # speak = speech_synthesizer.speak_text_async(response_text).get()
